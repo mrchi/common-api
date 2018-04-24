1853281c474f86fbab10e81ee1d4430b69c56f35
MrChi <chiqingjun@gmail.com>
2018-04-24 21:49:12 +0800

    rm doctest & optimize

--- source/base.py
+++ source/base.py
@@ -9,69 +9,20 @@ import requests
 
 
 class APIBase(object):
-    """自定义web请求类，封装requests
-
-    >>> base = APIBase()
-
-    >>> data = base._get("https://httpbin.org/get", params=dict(foo="bar"))
-
-    >>> data["url"]
-    'https://httpbin.org/get?foo=bar'
-
-    >>> data["args"]
-    {'foo': 'bar'}
-
-    >>> data = base._post("https://httpbin.org/post", json=dict(foo="bar"))
-
-    >>> data["url"]
-    'https://httpbin.org/post'
-
-    >>> data["data"]
-    '{"foo": "bar"}'
-
-    >>> data["headers"]["Content-Type"]
-    'application/json'
-
-    >>> data = base._post("https://httpbin.org/post", data=dict(foo="bar"))
-
-    >>> data["url"]
-    'https://httpbin.org/post'
-
-    >>> data["form"]
-    {'foo': 'bar'}
-
-    >>> data["headers"]["Content-Type"]
-    'application/x-www-form-urlencoded'
-
-    >>> data = base._put('https://httpbin.org/put', json=dict(foo="bar"))
-
-    >>> data["url"]
-    'https://httpbin.org/put'
-
-    >>> data["data"]
-    '{"foo": "bar"}'
-
-    >>> data["headers"]["Content-Type"]
-    'application/json'
-
-    >>> data = base._delete('https://httpbin.org/delete')
-
-    >>> data["url"]
-    'https://httpbin.org/delete'
-    """
+    """ 自定义web请求类，封装requests """
     def __init__(self, req=None):
         self._client = req or requests
         self.__connect_timeout = 5.0
         self.__socket_timeout = 5.0
         self.__proxies = {}
 
-    def set_connection_timeout(self, ms):
+    def set_connection_timeout(self, second):
         """设置请求超时时间，单位毫秒"""
-        self.__connect_timeout = ms / 1000.0
+        self.__connect_timeout = second
 
-    def set_socket_timeout(self, ms):
+    def set_socket_timeout(self, second):
         """设置响应超时时间，单位毫秒"""
-        self.__socket_timeout = ms / 1000.0
+        self.__socket_timeout = second
 
     def set_proxies(self, proxies):
         """设置代理"""
@@ -107,8 +58,3 @@ class APIBase(object):
     _post = partialmethod(_request, "POST")
     _put = partialmethod(_request, "PUT")
     _delete = partialmethod(_request, "DELETE")
-
-
-if __name__ == '__main__':
-    import doctest
-    doctest.testmod()
